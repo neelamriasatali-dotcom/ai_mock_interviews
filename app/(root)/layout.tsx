63ba03318ce6969d9a3701d90f0fc4b5ec8b531f
Neelam <neelam@example.com>
2025-11-19 06:36:37 -0800

    implement auth and storage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,25 @@
-import {ReactNode} from 'react'
-const Rootlayout = ({children}:{children: ReactNode}) => {
-  return (
-    <div>{children}</div>
-  )
-}
 
-export default Rootlayout
+import React, { ReactNode } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
+
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign-in");
+  return (
+    <div className="root-layout">
+      <nav className="flex items-center justify-between p-4">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+          <h2 className="text-primary-100 text-lg font-semibold">PrepWise</h2>
+        </Link>
+      </nav>
+
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default RootLayout;
