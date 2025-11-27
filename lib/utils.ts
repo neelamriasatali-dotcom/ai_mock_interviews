2cd3ebc376b2b629c9dbcdbe32df344937d1d07c
Neelam <neelam@example.com>
2025-11-26 22:45:26 -0800

    fix utils file path

--- lib/utils.ts
+++ lib/utils.ts
@@ -49,7 +49,10 @@
 
 
 
-import { interviewCovers, mappings } from "@/constants";
+// import { interviewCovers, mappings } from "@/constants";
+
+import { interviewCovers, mappings } from "../constants";
+
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
