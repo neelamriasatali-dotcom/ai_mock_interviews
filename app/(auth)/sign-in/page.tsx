8c8c5f7da0602def9eae1496bf946fcebeded67b
Neelam <neelam@example.com>
2025-12-08 23:28:51 -0800

    fix next-auth missing dependency

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import AuthForm from '@/components/AuthForm';   
-const page = () => {
+// import React from 'react'
+// import AuthForm from '@/components/AuthForm';   
+// const page = () => {
   
-return <AuthForm type="sign-in"/>
+// return <AuthForm type="sign-in"/>
   
-}
+// }
 
-export default page
+// export default page
+
+
+
+
+import React from "react";
+import AuthForm from "@/components/AuthForm";
+
+const SignInPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-full bg-black">
+      <AuthForm type ="sign-in" />
+    </div>
+  );
+};
+
+export default SignInPage;
