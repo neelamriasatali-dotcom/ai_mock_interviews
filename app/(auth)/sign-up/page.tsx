4323d61f6f207dd2955a6bb117cb24cd3ad23163
Neelam <neelam@example.com>
2025-12-08 21:28:13 -0800

    added signup dummy page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,18 @@
 
-import AuthForm from '@/components/AuthForm';
-const page = () => {
+// import AuthForm from '@/components/AuthForm';
+// const page = () => {
 
-    return <AuthForm type="sign-up"/>
+//     return <AuthForm type="sign-up"/>
   
-}
+// }
 
-export default page
+// export default page
+
+
+export default function SignUp() {
+  return (
+    <div style={{ color: "white" }}>
+      <h1>Sign Up</h1>
+    </div>
+  );
+}
